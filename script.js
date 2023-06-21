6be7b33ca2f759d9830abc8501d7635704175ec4
g1alg1 <136585504+g1alg1@users.noreply.github.com>
2023-06-21 09:49:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+// for(let i=0;i<10;i++){
+//     console.log("Hello world");
+// }
+// for(let i=0;i<10;i++){
+//     alert(i)
+// }
+// let a=+prompt("2+2=?")
+// switch(a){
+//     case 3:
+//     alert("Маловато");
+//     break;
+//     case 5:
+//     alert("перебор");
+//     break;
+//     case 4:
+//     alert("в точку");
+//     default:
+//         alert("нет таких значений");
+//         break;  
+// }
+// let a=+prompt("угадай число от 1 до 4")
+// switch(a){
+//     case 1:
+//         alert("проигрыш");
+//         break;
+//         case 2:
+//             alert("проигрыш");
+//             break;
+//             case 3:
+//                 alert("бесплатная поездка в Чернобыль");
+//                 break;
+//                 case 4:
+//                     alert("проигрыш");
+//                     break;
+//                     default:
+//                         alert("нет значений");
+//                         break;
+// }
+
+// let name = ['Лейла','Адам','Ахмед']
+// name.push ('Саид') || добовляет в конце массива
+// name.pop ( ) || удаляет  элемент из конца
+// name.shift ( ) || удаляет элемент из начала
+// name.unshift ('Магомед') || добавляет элемент в начало массива
+// name.splice (1,1) || добовляет,изменяет,заменяет
+// name slice (1,3) || возвращает новый массив
+
+// let name = ['Лейла','Адам','Ахмед',',Бага']
+// let a=name.slace (0,3)
+// alert (a)
+
+// let a=+prompt("Напишите число от 1 до 12")
+// switch(a){
+//         case 1:
+//             alert("Январь");
+//             break;
+//             case 2:
+//                 alert("Фефраль");
+//                 break;
+//                 case 3:
+//                     alert("Март");
+//                     break;
+//                     case 4:
+//                         alert("Апрель");
+//                         break;
+//                         case 5:
+//                             alert('Май');
+//                             break;
+//                         case 6:
+//                             alert("Июнь");
+//                             break;
+//                         case 7:
+//                             alert('Июль');
+//                             break;
+//                         case 8:
+//                             alert('Август');
+//                             break;
+//                         case 9:
+//                             alert('Сентябрь');
+//                             break;
+//                         case 10:
+//                             alert('Октябрь');
+//                             break;
+//                         case 11:
+//                             alert('Ноябрь');
+//                             break;
+//                         case 12:
+//                             alert('Деекабрь');
+//                             break;
+//                         default:
+//                             alert("нет значений");
+//                             break;
+//     }
+
+// function name (){
+//     alert('Лейла')
+// }
+//  name()
+
+// let a =document.querySelector('#text')
+// a.onclick=function(){
+//     a.style.color='red'
+// }
+
+// let a =document.querySelector('#text')
+// a.onclick=function(){
+//     a.style.color='red'
+//     a.style.fontSize='20px'
+//     a.innerHTML='Привет'
+// }
+
